ac2310e7e4ff94d80d6ef74eb3b87eea25767df1
Rafaela Silva <rafa_ss04@hotmail.com>
2021-03-20 10:51:42 -0300

    Criação da NavBar

---
+++ src/components/Header.vue
@@ -0,0 +1,58 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img src="../assets/logo.png" alt="New Dev Guide">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item" v-for="item in menu" :key="item.link">
+            <router-link class="nav-link active" :to="'/'+item.link">{{item.title}}</router-link>
+          </li>
+        </ul>
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Pesquise algo aqui" aria-label="Pesquisa">
+          <button class="btn btn-outline-primary" type="submit">Pesquisar</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+  img {
+    height: 3rem;
+  }
+</style>
+
+<script>
+export default {
+  data: () => ({
+    menu: [
+      {
+        title: 'Nosso propósito', 
+        link: 'proposito',
+      },
+      {
+        title: 'Guia iniciante', 
+        link: 'guia-iniciante',
+      },
+      {
+        title: 'Cursos', 
+        link: 'cursos-iniciantes',
+      },
+      {
+        title: 'Carreira', 
+        link: 'carreira',
+      },
+      {
+        title: 'Perfil', 
+        link: 'perfil',
+      },
+    ]
+  }),
+}
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,46 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+  },
+  {
+    path: '/proposito',
+    name: 'proposito',
+    component: () => import(/* webpackChunkName: "proposito" */ '../views/About.vue'),
+  },
+  {
+    path: '/guia-iniciante',
+    name: 'guia-iniciante',
+    component: () => import(/* webpackChunkName: "guia-iniciante" */ '../views/About.vue'),
+  },
+  {
+    path: '/cursos-iniciantes',
+    name: 'cursos-iniciantes',
+    component: () => import(/* webpackChunkName: "cursos-iniciantes" */ '../views/About.vue'),
+  },
+  {
+    path: '/carreira',
+    name: 'carreira',
+    component: () => import(/* webpackChunkName: "carreira" */ '../views/About.vue'),
+  },
+  {
+    path: '/perfil',
+    name: 'perfil',
+    component: () => import(/* webpackChunkName: "perfil" */ '../views/About.vue'),
+  },
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
